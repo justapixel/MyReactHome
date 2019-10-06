d1ebe2e1f4dcec96ac0b65278253c8925ca8553d
Rhuan Oliveira <rhuan.dev@gmail.com>
2019-10-06 15:05:52 -0300

    correction on sensors api

--- backend/src/controllers/SensorsController.js
+++ backend/src/controllers/SensorsController.js
@@ -2,6 +2,15 @@ const { Sensors } = require('../models');
 
 module.exports = {
   async store(req, res) {
-    return res.json();
+    const { location } = req.body;
+    const { type } = req.body;
+    const { state } = req.body;
+    const { value } = req.body;
+
+    Sensors.create({location: location.toUpperCase(), type: type.toUpperCase(),state: state, value: value}).then(resp => {
+      return res.json(resp.get({
+        plain: true
+      }))
+    });
   }
 };
